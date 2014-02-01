56665b7a162b2c6bfe645b69f52b0a432c665e88
june29 <june29.jp@gmail.com>
2014-02-01 15:34:58 +0900

    Emit 'blink' when keep on touching

--- public/js/huesync.js
+++ public/js/huesync.js
@@ -1,6 +1,6 @@
-var lastTouched = new Date();
-
 $(function() {
+  var timer;
+
   var socket = io.connect();
   socket.on('color', function (color) {
     $(document.body).css({ backgroundColor: color.code }).hide().fadeIn(300);
@@ -12,18 +12,20 @@ $(function() {
     });
   });
 
-  $(document.body).on("click touchstart", function() {
-    lastTouched = new Date();
+  $(document.body).on("touchstart", function() {
     console.log('touch detected');
+    socket.emit('touch');
+
+    if (timer) clearInterval(timer);
+    timer = setInterval(function() {
+      console.log('blink detected');
+      socket.emit('blink');
+    }, 1500);
   });
 
   $(document.body).on("touchend", function() {
-    if (new Date() - lastTouched > 2000) {
-      console.log('judged long touch: emit blink');
-      socket.emit('blink');
-    } else {
-      console.log('judged short touch: emit touch');
-      socket.emit('touch');
+    if (timer) {
+      clearInterval(timer);
     }
   });
 });
